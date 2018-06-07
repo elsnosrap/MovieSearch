ce01860c295f9968eeb77cf33abcd1b4d10d405a
Tim Parsons <tparsons@brightcove.com>
2018-06-07 19:54:24 -0400

    Move Activity to new ui package
    Keep activities out of main package

--- app/src/main/java/com/birdjunior/moviesearch/ui/SearchActivity.java
+++ app/src/main/java/com/birdjunior/moviesearch/ui/SearchActivity.java
@@ -1,7 +1,9 @@
-package com.birdjunior.moviesearch;
+package com.birdjunior.moviesearch.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.birdjunior.moviesearch.R;
 
 public class SearchActivity extends AppCompatActivity {
 
