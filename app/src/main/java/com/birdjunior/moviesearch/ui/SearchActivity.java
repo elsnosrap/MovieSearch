3dc65402c13a02dc52fd554c322becb0cfffdc3f
Tim Parsons <tparsons@brightcove.com>
2018-06-07 21:00:43 -0400

    Add search view
    Code that stubs out the search view

--- app/src/main/java/com/birdjunior/moviesearch/ui/SearchActivity.java
+++ app/src/main/java/com/birdjunior/moviesearch/ui/SearchActivity.java
@@ -1,7 +1,11 @@
 package com.birdjunior.moviesearch.ui;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
 
 import com.birdjunior.moviesearch.R;
 
@@ -11,5 +15,29 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        if (searchManager != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setMaxWidth(Integer.MAX_VALUE);
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+        }
+        return true;
     }
 }
