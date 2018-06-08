cce29308d63de51d16b10b5cd50a5e4d377a5a5f
Tim Parsons <tparsons@brightcove.com>
2018-06-08 17:36:05 -0400

    Add script to remove app from device
    Requires "shell-scripts" repo be cloned and configured per the readme:
    https://github.com/mitsnosrap/adb-shell-scripts

---
+++ remove.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script will remove various apps from a device
+
+# Apps to remove
+APP_NAME[0]="com.birdjunior.moviesearch"
+
+NUMAPPS=${#APP_NAME[@]}
+
+# Include the shell script that contains the function to select a device
+source $SHELL_SCRIPTS_HOME/adbwrapper-func.sh
+
+# Get the device to use for this command
+selectDevice SELECTED_DEVICE
+
+# Make sure the user selected a device
+if [[ "$SELECTED_DEVICE" = "0" ]]; then
+	echo "Please select a valid device"
+	exit 0;
+fi
+
+# Iterate through all defined apps and remove them
+for (( x=0; x < $NUMAPPS; x++ )); do
+	echo "Uninstalling ${APP_NAME[$x]} from device id $SELECTED_DEVICE"
+	adb -s $SELECTED_DEVICE uninstall ${APP_NAME[$x]} 2>/dev/null
+done
+
